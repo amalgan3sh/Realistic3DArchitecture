272de602300c5a5f36f4b2aeb893bda91cd11bc8
amal.ganesh <amal.ganesh@claysys.net>
2024-04-30 10:02:24 +0530

    database new

--- db/realistic3darchitecture_db.sql
+++ db/realistic3darchitecture_db.sql
@@ -1,6 +1,6 @@
 /*
-SQLyog Community v13.1.7 (64 bit)
-MySQL - 10.4.28-MariaDB : Database - realistic3darchitecture
+SQLyog Community v13.2.1 (64 bit)
+MySQL - 10.4.32-MariaDB : Database - realistic3darchitecture
 *********************************************************************
 */
 
@@ -26,12 +26,9 @@ CREATE TABLE `admin` (
   `password` varchar(20) DEFAULT NULL,
   `role` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`admin_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 /*Data for the table `admin` */
-
-insert  into `admin`(`admin_id`,`username`,`password`,`role`) values 
-(1,'admin','admin','admin');
 
 /*Table structure for table `architect` */
 
@@ -49,12 +46,15 @@ CREATE TABLE `architect` (
   `password` varchar(20) DEFAULT NULL,
   `status` varchar(10) DEFAULT 'active',
   PRIMARY KEY (`architecture_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 /*Data for the table `architect` */
 
 insert  into `architect`(`architecture_id`,`first_name`,`last_name`,`email`,`phone`,`certification`,`portfolio`,`username`,`password`,`status`) values 
-(1,NULL,NULL,'architect@gmail.com','1234512345',NULL,NULL,'architect','architect','active');
+(1,NULL,NULL,'architect@gmail.com','7859898698',NULL,NULL,'Anantha Krishnan','architect','active'),
+(2,NULL,NULL,'lijo@gmail.com','12312312',NULL,NULL,'Lijo Jose','lijo','active'),
+(3,NULL,NULL,'gouri@gmail.com','1234567812',NULL,NULL,'Gouri','gouri','active'),
+(4,NULL,NULL,'amal@gmail.com','7356363635',NULL,NULL,'Amal Neerad','amal','active');
 
 /*Table structure for table `company` */
 
@@ -71,12 +71,17 @@ CREATE TABLE `company` (
   `password` varchar(30) DEFAULT NULL,
   `status` varchar(10) DEFAULT 'active',
   PRIMARY KEY (`company_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 /*Data for the table `company` */
 
 insert  into `company`(`company_id`,`company_name`,`email`,`phone`,`pan_number`,`gst`,`username`,`password`,`status`) values 
-(1,NULL,'company@gmail.com','7894563214',NULL,NULL,'company','company','active');
+(1,NULL,'company@gmail.com','7898598589',NULL,NULL,'Horizon Builders','company','active'),
+(2,NULL,'company-1@gmail.com','7858986985',NULL,NULL,'SummitCraft Builders','company1','active'),
+(3,NULL,'marvel@gmail.com','1234567890',NULL,NULL,'Marvel Builders','marvel','active'),
+(4,NULL,'dcbuilders@gmail.com','1234567812',NULL,NULL,'DC builders','dcbuilders','active'),
+(5,NULL,'jackandjohn@gmail.com','1234567890',NULL,NULL,'Jack and John Building Co','jackandjohn','active'),
+(6,NULL,'jk@gmail.com','1234567890',NULL,NULL,'JK builders','jk123','active');
 
 /*Table structure for table `content` */
 
@@ -103,17 +108,13 @@ CREATE TABLE `design_model` (
   `project_id` int(11) DEFAULT NULL,
   `type` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`design_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 /*Data for the table `design_model` */
 
 insert  into `design_model`(`design_id`,`design_name`,`image`,`project_id`,`type`) values 
-(4,'test view','designs/pexels-sebastian-ervi-1763075.jpg',2,NULL),
-(5,'test view','designs/pexels-sebastian-ervi-17630751.jpg',2,NULL),
-(6,'project id test','designs/Screenshot_(7).png',2,NULL),
-(7,'project it test 2','designs/Screenshot_(7)1.png',NULL,NULL),
-(8,'project id test 3','designs/pexels-pixabay-46798.jpg',1,NULL),
-(9,'ahh','designs/pexels-fu-zhichao-587741.jpg',1,NULL);
+(1,'First Draft','designs/IMG_8982_(1).JPG',1,NULL),
+(2,'First Draft','designs/IMG_8977.JPG',1,NULL);
 
 /*Table structure for table `designers` */
 
@@ -131,12 +132,16 @@ CREATE TABLE `designers` (
   `password` varchar(30) DEFAULT NULL,
   `status` varbinary(10) DEFAULT 'active',
   PRIMARY KEY (`designer_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 /*Data for the table `designers` */
 
 insert  into `designers`(`designer_id`,`first_name`,`last_name`,`email`,`phone`,`certification`,`portfolio`,`username`,`password`,`status`) values 
-(1,NULL,NULL,'designer@gmail.com','7898985658',NULL,NULL,'Designer','designer','active');
+(1,NULL,NULL,'designer@gmail.com','7854587898',NULL,NULL,'Lijo Thekkethil','designer','active'),
+(2,NULL,NULL,'nivin@gmail.com','123123123',NULL,NULL,'nivin@gmail.com','nivin','active'),
+(3,NULL,NULL,'dulquer@gmail.com','1234512345',NULL,NULL,'dulquer','dulquer','active'),
+(4,NULL,NULL,'raju@gmail.com','1234123412',NULL,NULL,'Raju','user','active'),
+(5,NULL,NULL,'sanjay@gmail.com','1234561234',NULL,NULL,'Sanjay R','sanjay','active');
 
 /*Table structure for table `feedback` */
 
@@ -184,9 +189,12 @@ CREATE TABLE `portfolio` (
   `designer_id` int(11) DEFAULT NULL,
   `architecture_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`portfolio_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 /*Data for the table `portfolio` */
+
+insert  into `portfolio`(`portfolio_id`,`project_details`,`skills`,`education`,`experience`,`certifications`,`contact_informations`,`biography`,`additional_informations`,`designer_id`,`architecture_id`) values 
+(1,'rooftop cafe','designing','barc','2 year at x company','designer','1234567890','iam a professional  designer and have 2 year experience at x company','123',1,NULL);
 
 /*Table structure for table `project` */
 
@@ -196,19 +204,26 @@ CREATE TABLE `project` (
   `project_id` int(20) NOT NULL AUTO_INCREMENT,
   `project_name` varchar(30) DEFAULT NULL,
   `description` varchar(500) DEFAULT NULL,
+  `location` varchar(50) DEFAULT NULL,
+  `start_date` varchar(20) DEFAULT NULL,
+  `end_date` varchar(20) DEFAULT NULL,
+  `budget` varchar(30) DEFAULT NULL,
+  `materials_needed` varchar(200) DEFAULT NULL,
+  `special_requirments` varchar(300) DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
   `company_id` int(11) DEFAULT NULL,
   `designer_id` int(11) DEFAULT NULL,
   `architect_id` int(11) DEFAULT NULL,
   `status` varchar(20) DEFAULT 'pending',
   PRIMARY KEY (`project_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 /*Data for the table `project` */
 
-insert  into `project`(`project_id`,`project_name`,`description`,`user_id`,`company_id`,`designer_id`,`architect_id`,`status`) values 
-(1,'Urban Oasis: Sustainable Commu','Urban Oasis\" is a visionary project aimed at transforming urban spaces into sustainable, vibrant community hubs. This project encompasses the construction of a multi-functional complex designed to serve as a nexus of community engagement, environmental stewardship, and economic vitality',1,1,1,1,'accepted'),
-(2,'bakery shop','1000 sqr feet bakery at kalamaserry',1,1,NULL,NULL,'accepted');
+insert  into `project`(`project_id`,`project_name`,`description`,`location`,`start_date`,`end_date`,`budget`,`materials_needed`,`special_requirments`,`user_id`,`company_id`,`designer_id`,`architect_id`,`status`) values 
+(1,'Sunrise Villas','Construction of a residential complex with modern amenities.','123 Main Street, Cityville, State','2024-04-28','2024-11-28','50000000','Bricks, cement, steel rods, glass panels, plumbing fixtures','LEED certification, energy-efficient design, wheelchair accessibility',1,1,1,1,'accepted'),
+(2,'rooftop cafe','rooftop cafe at kalamassery','kalamassery','2024-12-12','2025-03-24','800000','furnitures, paints,cement,wood,wire','not yet decided',1,1,1,1,'accepted'),
+(3,'workshop','500sq ft workshop in x place ','near edapally xyz supermarket ','2024-05-23','2024-12-12','100000','xyz','xyz',1,4,NULL,NULL,'pending');
 
 /*Table structure for table `user` */
 
@@ -226,7 +241,7 @@ CREATE TABLE `user` (
 /*Data for the table `user` */
 
 insert  into `user`(`user_id`,`username`,`password`,`phone`,`email`) values 
-(1,'user','user','7356529545','user@gmail.com');
+(1,'Amal Ganesh','user','07356529545','user@gmail.com');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
